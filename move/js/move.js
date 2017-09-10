2f72fee7bc6daf9c064fb3ea776f1f2b746855ca
hongzzz <wuchanghongzzz@gmail.com>
2017-09-10 14:40:56 +0800

    -

--- move/js/move.js
+++ move/js/move.js
@@ -1,31 +1,35 @@
 function startMove(obj, json, fn) {
     clearInterval(obj.timer);
     obj.timer = setInterval(function () {
-        //属性是否全部运行完的标志
+        // 属性是否全部运行完的标志
         var flag = true;
         for (attr in json) {
-            //当前的值
-            var curattr = 0;
-            var target = json[attr];
+            // 当前的值
+            var curattr = 0,
+                target = json[attr];
+
             if (attr == 'opacity') {
                 curattr = Math.round(parseFloat(getStyle(obj, attr)) * 100);
             } else {
                 curattr = parseInt(getStyle(obj, attr));
             }
-            //计算速度
+
+            // 计算速度
             var speed = (target - curattr) / 8;
             speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-            //检测停止
+            // 检测停止
             if (curattr != json[attr]) {
                 flag = false;
             }
             if (attr == 'opacity') {
-                //obj.style.filter = 'alpha(opacity:' + (curattr + speed) + ')';
+                // obj.style.filter = 'alpha(opacity:' + (curattr + speed) + ')';
                 obj.style.opacity = (curattr + speed) / 100;
             }
             else {
                 obj.style[attr] = curattr + speed + 'px';
             }
+
+            // 判断是否完成
             if (flag) {
                 clearInterval(obj.timer);
                 if (fn) {
@@ -36,6 +40,7 @@ function startMove(obj, json, fn) {
     }, 30);
 }
 
+// 获取对象的属性值
 function getStyle(obj, attr) {
     if (obj.currentStyle) {
         return obj.currentStyle[attr];
