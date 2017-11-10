b1cba5f02c587e4bcc7e3b99e723c1cc9dfb286d
hongzzz <wuchanghongzzz@gmail.com>
2017-11-11 01:19:38 +0800

    change

--- js轮播图/js/main.js
+++ js轮播图/js/main.js
@@ -1,97 +1,79 @@
-var wrap = document.getElementsByClassName('wrap')[0];
-var prev = wrap.getElementsByClassName('prev')[0];
-var next = wrap.getElementsByClassName('next')[0];
-var imgsDiv = wrap.getElementsByClassName('imgs')[0];
-var img = imgsDiv.children[0];
-var btns = document.getElementsByClassName('btn')[0].getElementsByTagName('li');
+let wrap = document.getElementsByClassName('wrap')[0];
+let prev = wrap.getElementsByClassName('prev')[0];
+let next = wrap.getElementsByClassName('next')[0];
+let imgsDiv = wrap.getElementsByClassName('imgs')[0];
+let img = imgsDiv.children[0];
+let btns = document.getElementsByClassName('btn')[0].getElementsByTagName('li');
 
 // 自动播放
-var timer;
+let maxIndex = imgsDiv.offsetWidth / img.offsetWidth - 1;
+let index = 0;
 
-var maxindex = imgsDiv.offsetWidth / img.offsetWidth - 1;
-var index = -imgsDiv.offsetLeft / img.offsetWidth;
+imgsDiv.style.left = 0 + 'px';
 
 // 更新按钮状态
-function updatebutton() {
-    for (var a of btns) {
-        a.className = "";
-    }
-    if (index === maxindex) {
-        btns[0].className = "on";
-    } else {
-        btns[index].className = "on";
-    }
+function updateButtons() {
+  for (let a of btns) {
+    a.className = "";
+  }
+  btns[index].className = "on";
 }
 
-function moveright() {
-    index++;
-    var target = -index * img.offsetWidth;
-    startMove(imgsDiv, { left: target }, function () {
-        if (index === maxindex) {
-            index = 0;
-            imgsDiv.style.left = 0 + 'px';
-        }
-    });
-    updatebutton();
-    timer = setTimeout(moveright, 3000);
+// 按钮事件监听
+for (let i = 0; i < btns.length; i++) {
+  let btn = btns[i];
+  btn.setAttribute('data-index', i);
+  btn.addEventListener('click', function () {
+    for (let a of btns) {
+      a.className = "";
+    }
+    this.className = "on";
+    let index = this.getAttribute('data-index');
+    let target = -img.offsetWidth * index;
+    imgsDiv.style.left = target + 'px';
+  })
 }
 
-timer = setTimeout(moveright, 3000);
+let timer = setInterval(function () {
+  move('right');
+}, 3000);
 
+wrap.addEventListener('mouseout', function () {
+  timer = setInterval(function () {
+    move('right');
+  }, 3000);
+});
 
 // 指的时候停止滚动
 wrap.addEventListener('mouseover', function () {
-    clearTimeout(timer);
+  clearInterval(timer);
 });
 
-wrap.addEventListener('mouseout', function () {
-    timer = setTimeout(moveright, 3000);
-})
 
-
-// 按钮事件监听
-for (var i = 0; i < btns.length; i++) {
-    btn = btns[i];
-    btn.setAttribute('data-index', i);
-    btn.addEventListener('click', function () {
-        for (var a of btns) {
-            a.className = "";
-        }
-        this.className = "on";
-        var index = this.getAttribute('data-index');
-        var target = -img.offsetWidth * index;
-        startMove(imgsDiv, { left: target });
-    })
+function move(type) {
+  if (type === 'right') {
+    index++;
+  } else if (type === 'left') {
+    index--;
+  }
+  if (index === maxIndex) {
+    index = 0;
+  }
+  if (index < 0) {
+    index = maxIndex - 1;
+  }
+  let target = -index * img.offsetWidth;
+  imgsDiv.style.left = target + 'px';
+  updateButtons();
 }
 
 // 左按钮
 prev.addEventListener('click', function () {
-    index--;
-    if (index < 0) {
-        index = maxindex;
-        imgsDiv.style.left = -index * img.offsetWidth + 'px';
-        index--;
-    }
-
-    var target = -index * img.offsetWidth;
-    startMove(imgsDiv, { left: target });
-    updatebutton();
-})
+  move('left');
+});
 
 // 右按钮
 next.addEventListener('click', function () {
-    index++;
-    if (index > maxindex) {
-        index = maxindex;
-    }
-
-    var target = -index * img.offsetWidth;
-    startMove(imgsDiv, { left: target }, function () {
-        if (index === maxindex) {
-            index = 0;
-            imgsDiv.style.left = 0 + 'px';
-        }
-    });
-    updatebutton();
-})
+  move('right');
+});
 
